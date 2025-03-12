cd3b1b532d6a8d14a308b57eba8c98ca292db305
DanNicolaeBoca <dan.nicolae.boca01@gmail.com>
2025-03-13 00:04:18 +0200

    Added basic functionality
    Could add duplicates check and message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,43 @@
+import { NgClass, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+
+export interface TodoItem {
+  id: number;
+  task: string;
+  completed: boolean;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, FormsModule, NgFor, NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular-todo-app';
+  todoList: TodoItem[] = [{id: 0, task: 'Learn Angular', completed: false}];
+  newTask: string = '';
+
+  addTask(): void {
+    if (this.newTask.trim() !== '') {
+
+      const newTodoItem: TodoItem = {
+        id: Date.now(),
+        task: this.newTask,
+        completed: false
+      }
+
+      this.todoList.push(newTodoItem);
+      this.newTask = '';
+    }
+  }
+  toggleCompleted(index: number): void {
+    this.todoList[index].completed = !this.todoList[index].completed;
+  }
+  deleteTask(id: number): void {
+    this.todoList = this.todoList.filter(item => item.id !== id);
+    console.log(this.todoList);
+  }
 }
